59dbb36db1bc2553e44a586a5e67d4bb14c2b6dc
Yan-jianghua <1784582807@qq.com>
2026-09-04 18:17:10 +0800

    Create day_1.py

---
+++ docs/01-python-basics/day_1.py
@@ -0,0 +1,14 @@
+name="小华"
+id=6918
+birthday="2006-12-18"
+
+print(name,id,birthday)
+a=input("请输入你要给她的留言:")
+b=int(input("你希望他什么时候可查看留言：（小时）"))
+
+
+print(f"经过了{b}小时")
+print(f"{name}已阅读留言")
+print(type(birthday))
+C=2
+print(C+b)
